36146932f2d7ade9a85d46869ba2d6a8fcaa6c27
Alexandra Vlasova <Alex.Genrietta@gmail.com>
2021-08-15 18:54:01 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let strDecoded = '';
+    let letter;
+
+    while (expr.length > 0) {
+        letter = expr.slice(0, 10);
+        strDecoded += decodeLetter(letter);
+        expr = expr.slice(10);
+    }
+
+    return strDecoded;
+}
+
+function decodeLetter(code) {
+    if (code === '**********')
+        return ' ';
+
+    let codeMorse = '';
+    let sign = '';
+
+    for (let i = 0; i < code.length; i = i + 2) {
+        sign = code[i] + code[i + 1];
+
+        switch (sign) {
+            case '10':
+                codeMorse += '.';
+                break;
+            case '11':
+                codeMorse += '-';
+                break;
+            default:
+                break;
+        }
+    }
+
+    return MORSE_TABLE[codeMorse];
 }
 
 module.exports = {
